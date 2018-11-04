ccf62d36f19e0e8c72cc962006a1db8d889b901f
中嶋　慎也 <nakasin52@gmail.com>
2018-11-04 17:16:50 +0900

    Qiitaサイトで入門中
    https://qiita.com/sekiyaeiji/items/f1df5d9d4c8a147eed77

--- vue/main.js
+++ vue/main.js
@@ -1,14 +1,49 @@
 ﻿function initial(){
+	new Vue({
+		el: '#component01',
+		data:{
+			text: 'テスト用入力値'
+		}
+	});
 
 	new Vue({
-	  el: '#example',
+	  el: '#component02',
 	  data: {
-	    message: 'Hello'
+	    text: ''
+	  }
+	});
+
+	new Vue({
+	  el: '#component03',
+	  data: {
+	    input: '',
+	    text: ''
 	  },
-		computed: {
-  		reversedMessage: function () {
-    		return this.message.split('').reverse().join('')
-  		}
-		}
-	})
+	  methods: {
+	    output: function () {
+	      this.text = this.input;
+	    }
+	  }
+	});
+
+	new Vue({
+	  el: '#component04',
+	  data: {
+	    input: '',
+	    html: ''
+	  },
+	  methods: {
+	    output: function () {
+	      this.html = '<span style="color: #fc0;">' + this.input + '</span>';
+	    }
+	  }
+	});
+
+	new Vue({
+	  el: '#component05',
+	  data: {
+	    number: ''
+	  }
+	});
+
 }
